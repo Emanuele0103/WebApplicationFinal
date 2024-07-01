55a7935fd281d67b805b066f06afded2fb98b63b
Emanuele <emanuelegrande2@gmail.com>
2024-07-01 15:20:23 +0200

    Update script in all HTML

--- src/main/resources/static/css/js/profile.js
+++ src/main/resources/static/css/js/profile.js
@@ -7,24 +7,53 @@ $(document).ready(function() {
         window.location.href = '/my_announcements';
     });
 
-    $('#logout').click(function() {
-        $.ajax({
-            url: '/auth/doLogout', // Chiamata all'endpoint di logout nel tuo controller Spring Boot
-            method: 'GET',
-            success: function(response) {
-                alert('Logout effettuato con successo!');
-                deleteSessionCookie(); // Rimuove il cookie di sessione dal frontend
-                window.location.href = '/'; // Reindirizza alla homepage dopo il logout
-            },
-            error: function(xhr, status, error) {
-                console.error('Errore durante il logout:', error);
-                alert('Errore durante il logout. Si prega di riprovare.');
+    $(document).ready(function() {
+        // Funzione per verificare lo stato di login e aggiornare il pulsante
+        function checkAndUpdateLoginStatus() {
+            var sessionToken = localStorage.getItem('sessionToken');
+            if (sessionToken) {
+                $.ajax({
+                    url: '/auth/checkLogin',
+                    method: 'GET',
+                    headers: { 'Authorization': 'Bearer ' + sessionToken },
+                    success: function(response) {
+                        if (response) {
+                            $('#user-button').text('Profilo').attr('href', '/profile');
+                        } else {
+                            localStorage.removeItem('sessionToken');
+                            $('#user-button').text('Accedi').attr('href', 'accesso');
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Errore durante la verifica dello stato di login:', error);
+                    }
+                });
+            } else {
+                $('#user-button').text('Accedi').attr('href', 'accesso');
             }
+        }
+
+        // Chiamata per verificare lo stato di login quando la pagina è caricata
+        checkAndUpdateLoginStatus();
+
+        // Evento click sul pulsante di logout
+        $('#logout').click(function(event) {
+            event.preventDefault();
+            $.ajax({
+                url: '/auth/doLogout',
+                method: 'GET',
+                success: function(response) {
+                    console.log(response);
+                    localStorage.removeItem('sessionToken');
+                    $('#user-button').text('Accedi').attr('href', 'accesso');
+                    alert('Logout effettuato con successo!');
+                    window.location.href = '/'; // Reindirizza alla homepage dopo il logout
+                },
+                error: function(xhr, status, error) {
+                    console.error('Errore durante il logout:', error);
+                    alert('Errore durante il logout. Si prega di riprovare.');
+                }
+            });
         });
     });
-
-// Funzione per eliminare il cookie di sessione dal frontend
-    function deleteSessionCookie() {
-        document.cookie = "sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    }
 });
